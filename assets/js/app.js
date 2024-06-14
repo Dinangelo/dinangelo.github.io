c8064d4aa756d0383ceea8e7f5d6350b327eb1a9
Dinangelo <dravalijaona@gmail.com>
2024-06-14 17:34:37 +0300

    Ajout responsive

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector(".menu-humburger");
+const nav = document.querySelector(".nav-links");
+
+menu.addEventListener('click',()=>{
+    nav.classList.toggle('menu-respons')
+})
