843d7cd5c21882a3d84499c3caa3f2c88d89470e
Dinangelo <dravalijaona@gmail.com>
2024-06-28 11:46:09 +0300

    Refonte

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,32 @@ const nav = document.querySelector(".nav-links");
 menu.addEventListener('click',()=>{
     nav.classList.toggle('menu-respons')
 })
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const textElement = document.getElementById('animatedText');
+//     const text = textElement.textContent;
+//     textElement.textContent = '';
+
+//     for (let char of text) {
+//         const span = document.createElement('span');
+//         span.textContent = char;
+//         textElement.appendChild(span);
+//     }
+
+//     let index = 0;
+//     const spans = textElement.querySelectorAll('span');
+
+//     function animate() {
+//         if (index > 0) {
+//             spans[index - 1].classList.remove('active');
+//         }
+//         if (index < spans.length) {
+//             spans[index].classList.add('active');
+//             index++;
+//         } else {
+//             index = 0;
+//         }
+//     }
+
+//     setInterval(animate, 500);
+// });
