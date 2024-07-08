449e37c1fc7d864bfbd6a3b86fee5568286c2692
Dinangelo <dravalijaona@gmail.com>
2024-07-08 11:15:23 +0300

    Fanatsarana

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,4 +3,9 @@ const nav = document.querySelector(".nav-links");
 
 menu.addEventListener('click',()=>{
     nav.classList.toggle('menu-respons')
-})
+});
+
+document.addEventListener("DOMContentLoaded",function() {
+    var annee = new Date().getFullYear();
+    document.getElementById("annee").textContent = annee;
+});
